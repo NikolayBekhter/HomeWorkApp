4358ebbdc4683bbcd621e639320142f712e3a246
NikolayBekhter <n.v.bekhter@mail.ru>
2022-01-16 21:16:13 +0300

    Homework lesson 2.2 made new changes

--- src/main/java/ru/geekbrains/java_level_1/Lesson_2.java
+++ src/main/java/ru/geekbrains/java_level_1/Lesson_2.java
@@ -4,7 +4,7 @@ public class Lesson_2 {
 
     public static void main(String[] args) {
         System.out.println(leapYearOrNot(2022));
-        System.out.println(checkingSum(7, 13));
+        System.out.println(checkingSum(8, 7));
         positiveOrNegativeCase1(5);
         System.out.println(positiveOrNegativeCase2(-5));
         printLine("Java", 3);
@@ -26,11 +26,7 @@ public class Lesson_2 {
     }
 
     public static boolean checkingSum(int a, int b) {
-        boolean check = false;
-        if (a + b >= 10 && a + b <= 20) {
-            check = true;
-        }
-        return false;
+        return (a + b >= 10 && a + b <= 20);
     }
 
     public static void positiveOrNegativeCase1(int a) {
@@ -42,11 +38,7 @@ public class Lesson_2 {
     }
 
     public static boolean positiveOrNegativeCase2(int a) {
-        boolean result = true;
-        if (a >= 0) {
-            result = false;
-        }
-        return result;
+        return (a < 0);
     }
 
     public static void printLine(String str, int a) {
