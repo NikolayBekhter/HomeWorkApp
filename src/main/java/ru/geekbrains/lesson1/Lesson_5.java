af26a9875cd5069f0e559d855b0c7d48963dea26
NikolayBekhter <n.v.bekhter@mail.ru>
2022-01-28 19:29:41 +0300

    Homework lesson 5.1

--- src/main/java/ru/geekbrains/lesson1/Lesson_5.java
+++ src/main/java/ru/geekbrains/lesson1/Lesson_5.java
@@ -1,4 +1,22 @@
 package ru.geekbrains.lesson1;
 
 public class Lesson_5 {
+
+    public static void main(String[] args) {
+
+        Person[] personArray = new Person[5];
+        personArray[0] = new Person("Бехин Николай", "Ведущий СКС", "kolya@mang.ru",
+                              "89125435670", 3000, 43);
+        personArray[1] = new Person("Клюев Андрей", "Главный СКС", "andrey@mang.ru",
+                              "89125435671", 2500, 41);
+        personArray[2] = new Person("Ермаков Евгений", "СКС", "evgeniy@mang.ru",
+                              "89125435672", 2000, 35);
+        personArray[3] = new Person("Семин Юрий", "СКС", "uriy@mang.ru",
+                              "89125435673", 2100, 36);
+        personArray[4] = new Person("Коваль Семён", "СКС", "semen@mang.ru",
+                              "89125435674", 1900, 39);
+
+        for (int i=0; i < personArray.length; i++)
+            if (personArray[i].getAge() > 40) personArray[i].print();
+    }
 }
