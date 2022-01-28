191fb6311156fa1abd33448d3a971c7cb5005d7a
NikolayBekhter <n.v.bekhter@mail.ru>
2022-01-28 19:29:41 +0300

    Homework lesson 5.1

---
+++ src/main/java/ru/geekbrains/lesson1/Person.java
@@ -0,0 +1,74 @@
+package ru.geekbrains.lesson1;
+
+public class Person {
+
+    private String name;
+    private String position;
+    private String email;
+    private String phone_number;
+    private int salary;
+    private int age;
+
+
+    public Person(String name, String position, String email, String phone_number, int salary, int age) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone_number = phone_number;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() { return age; }
+
+    public void setAge(int age) { this.age = age; }
+
+    @Override
+    public String toString(){
+        return String.format("Имя: %s \tДолжность: %s \nEmail: %s \t Номер телефона: %s \n Зарплата: %d \t Возраст: %d \n",
+                name, position, email, phone_number, salary, age);}
+
+    public void print(){
+        System.out.println(this);
+    }
+}
